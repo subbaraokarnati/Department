1f562d218670c9791b83783abf4c485d6de6fe83
Karnati Subbarao <karnati.subbarao@ibm.com>
2024-06-14 13:09:03 +0530

    first commit

---
+++ src/main/java/com/department/controller/DepartmentController.java
@@ -0,0 +1,34 @@
+package com.department.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.department.entity.Department;
+import com.department.service.DepartmentService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/departments")
+@Slf4j
+public class DepartmentController {
+	
+	@Autowired
+	private DepartmentService departmentService;
+	
+	@PostMapping("/")
+	public Department savDepartment(@RequestBody Department department) {
+		
+		return departmentService.saveDepartment(department);
+	}
+	
+	@GetMapping("/{id}")
+	public Department findDepartmentById(@PathVariable("id") Long departmentId) {
+		return departmentService.findDepartmentById(departmentId);
+	}
+}
---
+++ src/main/java/com/department/entity/Department.java
@@ -0,0 +1,78 @@
+package com.department.entity;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
+public class Department {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long departmentId;
+	private String departmentName;
+	private String departmentAddress;
+	private String departmentCode;
+	public Long getDepartmentId() {
+		return departmentId;
+	}
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
+	}
+	public String getDepartmentName() {
+		return departmentName;
+	}
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	public String getDepartmentAddress() {
+		return departmentAddress;
+	}
+	public void setDepartmentAddress(String departmentAddress) {
+		this.departmentAddress = departmentAddress;
+	}
+	public String getDepartmentCode() {
+		return departmentCode;
+	}
+	public void setDepartmentCode(String departmentCode) {
+		this.departmentCode = departmentCode;
+	}
+	@Override
+	public String toString() {
+		return "Department [departmentId=" + departmentId + ", departmentName=" + departmentName
+				+ ", departmentAddress=" + departmentAddress + ", departmentCode=" + departmentCode + "]";
+	}
+	public Department(Long departmentId, String departmentName, String departmentAddress, String departmentCode) {
+		super();
+		this.departmentId = departmentId;
+		this.departmentName = departmentName;
+		this.departmentAddress = departmentAddress;
+		this.departmentCode = departmentCode;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(departmentAddress, departmentCode, departmentId, departmentName);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		return Objects.equals(departmentAddress, other.departmentAddress)
+				&& Objects.equals(departmentCode, other.departmentCode)
+				&& Objects.equals(departmentId, other.departmentId)
+				&& Objects.equals(departmentName, other.departmentName);
+	}
+	public Department() {
+		super();
+	}
+	
+	
+}
